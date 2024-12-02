969702de5ebaf517671fdaf676b19757a7cd3441
Gabriel <gabrielraimundo@id.uff.br>
2024-12-02 13:34:04 -0300

    Initial commit

---
+++ src/main/java/com/whiteStudio/Ecommerce_Platform_Spring/EcommercePlatformSpringApplication.java
@@ -0,0 +1,13 @@
+package com.whiteStudio.Ecommerce_Platform_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePlatformSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whiteStudio/Ecommerce_Platform_Spring/EcommercePlatformSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteStudio.Ecommerce_Platform_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
