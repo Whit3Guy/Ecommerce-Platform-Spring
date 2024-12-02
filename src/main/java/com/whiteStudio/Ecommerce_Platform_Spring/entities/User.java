ee2f457ed01574cff785a55fd99847962d1ac2d9
Gabriel <gabrielraimundo@id.uff.br>
2024-12-02 17:57:58 -0300

    Entity user and jpa connection

--- src/main/java/com/whiteStudio/Ecommerce_Platform_Spring/entities/User.java
+++ src/main/java/com/whiteStudio/Ecommerce_Platform_Spring/entities/User.java
@@ -1,14 +1,15 @@
 package com.whiteStudio.Ecommerce_Platform_Spring.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "user_table")
 public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
